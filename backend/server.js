f5081c909b59877f531e3639e4e96109ce94a21e
dryi02 <dryi@uci.edu>
2024-11-13 18:05:06 -0800

    backend changes 2

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const connectDB = require('./config/db');
 const classRoutes = require('./routes/classRoutes');
 const pledgeRoutes = require('./routes/pledgeRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const { createServer } = require('@vercel/node');
 const app = express();
+
 
 // Load environment variables
 require('dotenv').config();
@@ -31,5 +31,10 @@ app.get('/', (req, res) => {
   res.status(200).send('SEP Tracker Backend');
 });
 
-// Export the Express app as a Vercel serverless function
-module.exports = createServer(app);
+const PORT = process.env.PORT || 5000; // You can use any port you want
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Export the Express app directly for use with Vercel
+module.exports = app;
