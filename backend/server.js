a02005826421df5ccb98cdda02565c7af8718c1e
dryi02 <dryi@uci.edu>
2024-11-18 13:38:03 -0800

    adding a /

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://sep-fund-tracker.vercel.app'],
+  origin: ['http://localhost:5173/', 'https://sep-fund-tracker.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
